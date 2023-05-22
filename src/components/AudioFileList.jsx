7166db597660e24643b3a87e67442ea2e0fa86fa
Nicholas Bazzoni <nico@Nicholass-MBP.fios-router.home>
2023-05-22 14:07:33 -0400

    better UI

--- src/components/AudioFileList.jsx
+++ src/components/AudioFileList.jsx
@@ -3,14 +3,11 @@ import { firestore } from '../../firebase';
 import { format } from 'date-fns';
 import ReactAudioPlayer from 'react-audio-player';
 
-
 const AudioFileList = () => {
   const [audioFiles, setAudioFiles] = useState([]);
 
-  //collect audioFiles from firestore
   useEffect(() => {
     const unsubscribe = firestore
-
       .collection('audioFiles')
       .orderBy('createdAt', 'desc')
       .onSnapshot((snapshot) => {
@@ -25,42 +22,43 @@ const AudioFileList = () => {
     return () => unsubscribe();
   }, []);
 
-
   return (
-    <div>
-      <h2 className="text-2xl text-blue-200 p-1   font-mono font-bold mb-1">Yaps:</h2>
-      {audioFiles.map((file) => (
-        <div key={file.id} className="mb-4 space-y-4  space-x-3 p-1">
-          <div className="flex border-t-2  justify-between items-center">
-            <div
-              className="bg-cover bg-center mt-2 shadow-slate-400 shadow-lg w-10 h-10 rounded-full"
+    <div className="container mx-auto mt-8">
+      <h2 className="text-2xl text-blue-200 p-1 font-mono font-bold mb-1">Yaps:</h2>
+      <div className="grid grid-cols-1 md:grid-cols-3 lg:grid-cols-3 gap-4">
+        {audioFiles.map((file) => (
+          <div key={file.id} className="mb-4 space-y-4  space-x-3 p-1">
+            <div className="flex border-t-2 justify-between items-center">
+              <div
+                className="bg-cover bg-center mt-2 shadow-slate-400 shadow-lg w-10 h-10 rounded-full"
+                style={{
+                  backgroundImage: `url(${file.photo})`,
+                }}
+              ></div>
+              <p className="text-gray-600 text-xs bg-slate-50 font-mono shadow-md rounded-full max-w-fit p-2">
+                {file.username}
+              </p>
+            </div>
+            <ReactAudioPlayer
+              src={file.url}
+              className="mb-2"
+              controls
               style={{
-                backgroundImage: `url(${file.photo})`,
+                width: '100%',
+                borderRadius: '8px',
+                padding: '8px',
+                backgroundColor: '#ffffff',
+                boxShadow: '0 0 10px rgba(0, 0, 0, 0.2)',
               }}
-            ></div>
-            <p className="text-gray-600 text-xs bg-slate-50 font-mono shadow-md rounded-full max-w-fit p-2">
-              {file.username}
-            </p>
+            />
+            {file.createdAt && (
+              <p className="text-gray-600 bg-whitesmoke font-mono text-xs p-1">
+                {format(file.createdAt.toDate(), 'MM·dd·yy - h:mm')}
+              </p>
+            )}
           </div>
-          <ReactAudioPlayer
-            src={file.url}
-            className="mb-2"
-            controls
-            style={{
-              width: '100%',
-              borderRadius: '8px',
-              padding: '8px',
-              backgroundColor: '#ffffff',
-              boxShadow: '0 0 10px rgba(0, 0, 0, 0.2)',
-            }}
-          />
-          {file.createdAt && (
-            <p className="text-gray-600 bg-whitesmoke font-mono text-xs p-1">
-              {format(file.createdAt.toDate(), 'MM·dd·yy - h:mm')}
-            </p>
-          )}
-        </div>
-      ))}
+        ))}
+      </div>
     </div>
   );
 };
--- src/components/Chat.jsx
+++ src/components/Chat.jsx
@@ -119,27 +119,25 @@ const Chat = () => {
   
 
   return (
-    <div className="flex flex-col space-y-4 md:flex-row md:space-y-0 md:space-x-4 mt-4">
-      <div className="overflow-x-hidden space-x-4 ">
-        <h2 className="text-2xl font-bold mb-4">Chat</h2>
-        {currentUser && user && user.username && user.photo && (
-          <h3 className="text-lg font-semibold mb-2">Logged in as: {user.username}</h3>
-        )}
-        <div className="flex items-center  space-x-4 justify-space-between  ">
-        <button
-    className={`bg-blue-500 hover:bg-blue-600 text-white py-2 px-4 rounded focus:outline-none ${
-      isRecording ? 'animate-pulse' : ''
-    }`}
-    onClick={handleStartRecording}
-  >
-   talk
-  </button>
-  <button
-    className="bg-red-500 hover:bg-red-600 text-white py-2 px-4 rounded focus:outline-none"
-    onClick={handleStopRecording}
-  >
-    stop
-  </button>
+    <div className="flex flex-col items-center mt-8">
+      <h2 className="text-2xl font-bold mb-4">Chat</h2>
+      {currentUser && user && user.username && user.photo && (
+        <h3 className="text-lg font-semibold mb-2">Logged in as: {user.username}</h3>
+      )}
+      <div className="flex flex-col md:flex-row items-center space-y-4 md:space-y-0 md:space-x-4">
+        <div className="flex space-x-4">
+          <button
+            className="bg-blue-500 hover:bg-blue-600 text-white py-2 px-4 rounded focus:outline-none"
+            onClick={handleStartRecording}
+          >
+            talk
+          </button>
+          <button
+            className="bg-red-500 hover:bg-red-600 text-white py-2 px-4 rounded focus:outline-none"
+            onClick={handleStopRecording}
+          >
+            stop
+          </button>
           <button
             className="bg-green-500 hover:bg-green-600 text-white py-2 px-4 rounded focus:outline-none"
             onClick={handlePlayback}
@@ -147,24 +145,31 @@ const Chat = () => {
             listen
           </button>
           <button
-            className="bg-yellow-300  hover:bg-rose-600 text-black py-2 px-4 rounded focus:outline-none"
+            className="bg-yellow-300 hover:bg-rose-600 text-white py-2 px-4 rounded focus:outline-none"
             onClick={handleSave}
           >
-            save
+            Save
           </button>
         </div>
+        {audioRecording && (
+          <audio
+            ref={audioPlayerRef}
+            src={URL.createObjectURL(audioRecording)}
+            controls
+          />
+        )}
       </div>
-      {audioRecording && (
-        <audio
-          ref={audioPlayerRef}
-          src={URL.createObjectURL(audioRecording)}
-        />
-      )}
       <AudioFileList />
     </div>
   );
-  
-  
 };
 
 export default Chat;
+
+
+
+
+
+
+
+
